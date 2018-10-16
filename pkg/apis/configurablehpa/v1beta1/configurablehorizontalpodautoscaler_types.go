2570b5530a080d8a03b17f67383c1d9aaf1069f1
Ivan Glushkov <ivan@postmates.com>
2018-10-16 18:44:49 +0400

    added types, some basic prints + fixed tests

--- pkg/apis/configurablehpa/v1beta1/configurablehorizontalpodautoscaler_types.go
+++ pkg/apis/configurablehpa/v1beta1/configurablehorizontalpodautoscaler_types.go
@@ -20,19 +20,32 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// CrossVersionObjectReference contains enough information to let you identify the referred resource.
+type CrossVersionObjectReference struct {
+	// Kind of the referent; More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds"
+	Kind string `json:"kind"`
+	// Name of the referent; More info: http://kubernetes.io/docs/user-guide/identifiers#names
+	Name string `json:"name"`
+	// API version of the referent
+	// +optional
+	APIVersion string `json:"apiVersion,omitempty"`
+}
 
 // ConfigurableHorizontalPodAutoscalerSpec defines the desired state of ConfigurableHorizontalPodAutoscaler
 type ConfigurableHorizontalPodAutoscalerSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	DownscaleStabilizationWindowSeconds  int32   `json:"downscaleStabilizationWindow"`
+	DownscaleLimit                       int32   `json:"downscaleLimit,omitempty"`
+	Tolerance                            float64 `json:"tolerance,omitempty"`
+	CpuInitializationPeriodSeconds       int32   `json:"cpuInitializationPeriod,omitempty"`
+	DelayOfInitialReadinessStatusSeconds int32   `json:"delayOfInitialReadinessStatus,omitempty"`
 }
 
 // ConfigurableHorizontalPodAutoscalerStatus defines the observed state of ConfigurableHorizontalPodAutoscaler
 type ConfigurableHorizontalPodAutoscalerStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	ObservedGeneration *int64       `json:"observedGeneration,omitempty"`
+	LastScaleTime      *metav1.Time `json:"lastScaleTime,omitempty"`
+	CurrentReplicas    int32        `json:"currentReplicas"`
+	DesiredReplicas    int32        `json:"desiredReplicas"`
 }
 
 // +genclient
--- pkg/controller/configurablehorizontalpodautoscaler/configurablehorizontalpodautoscaler_controller.go
+++ pkg/controller/configurablehorizontalpodautoscaler/configurablehorizontalpodautoscaler_controller.go
@@ -19,32 +19,23 @@ package configurablehorizontalpodautoscaler
 import (
 	"context"
 	"log"
-	"reflect"
+	"time"
 
 	configurablehpav1beta1 "github.com/postmates/configurable-hpa/pkg/apis/configurablehpa/v1beta1"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
+const defaultSyncPeriod = time.Second * 15
 
 // Add creates a new ConfigurableHorizontalPodAutoscaler Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
-// USER ACTION REQUIRED: update cmd/manager/main.go to call this configurablehpa.Add(mgr) to install this Controller
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
 }
@@ -68,16 +59,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create
-	// Uncomment watch a Deployment created by ConfigurableHorizontalPodAutoscaler - change this for objects you create
-	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &configurablehpav1beta1.ConfigurableHorizontalPodAutoscaler{},
-	})
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
@@ -86,18 +67,20 @@ var _ reconcile.Reconciler = &ReconcileConfigurableHorizontalPodAutoscaler{}
 // ReconcileConfigurableHorizontalPodAutoscaler reconciles a ConfigurableHorizontalPodAutoscaler object
 type ReconcileConfigurableHorizontalPodAutoscaler struct {
 	client.Client
-	scheme *runtime.Scheme
+	scheme     *runtime.Scheme
+	syncPeriod time.Duration
 }
 
 // Reconcile reads that state of the cluster for a ConfigurableHorizontalPodAutoscaler object and makes changes based on the state read
 // and what is in the ConfigurableHorizontalPodAutoscaler.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  The scaffolding writes
-// a Deployment as an example
 // Automatically generate RBAC rules to allow the Controller to read and write Deployments
-// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// TODO: decide, what to use: patch or update
+// +kubebuilder:rbac:groups=apps,resources=replicasets,verbs=get;list;update;patch
+// +kubebuilder:rbac:groups=apps,resources=pods,verbs=get;list;create;update;patch;delete
 // +kubebuilder:rbac:groups=configurablehpa.k8s.io,resources=configurablehorizontalpodautoscalers,verbs=get;list;watch;create;update;patch;delete
 func (r *ReconcileConfigurableHorizontalPodAutoscaler) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	// Fetch the ConfigurableHorizontalPodAutoscaler instance
+	log.Printf("Reconcile request: %v\n", request)
 	instance := &configurablehpav1beta1.ConfigurableHorizontalPodAutoscaler{}
 	err := r.Get(context.TODO(), request.NamespacedName, instance)
 	if err != nil {
@@ -110,57 +93,6 @@ func (r *ReconcileConfigurableHorizontalPodAutoscaler) Reconcile(request reconci
 		return reconcile.Result{}, err
 	}
 
-	// TODO(user): Change this to be the object type created by your controller
-	// Define the desired Deployment object
-	deploy := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      instance.Name + "-deployment",
-			Namespace: instance.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{"deployment": instance.Name + "-deployment"},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": instance.Name + "-deployment"}},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  "nginx",
-							Image: "nginx",
-						},
-					},
-				},
-			},
-		},
-	}
-	if err := controllerutil.SetControllerReference(instance, deploy, r.scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// TODO(user): Change this for the object type created by your controller
-	// Check if the Deployment already exists
-	found := &appsv1.Deployment{}
-	err = r.Get(context.TODO(), types.NamespacedName{Name: deploy.Name, Namespace: deploy.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		log.Printf("Creating Deployment %s/%s\n", deploy.Namespace, deploy.Name)
-		err = r.Create(context.TODO(), deploy)
-		if err != nil {
-			return reconcile.Result{}, err
-		}
-	} else if err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// TODO(user): Change this for the object type created by your controller
-	// Update the found object and write the result back if there are any changes
-	if !reflect.DeepEqual(deploy.Spec, found.Spec) {
-		found.Spec = deploy.Spec
-		log.Printf("Updating Deployment %s/%s\n", deploy.Namespace, deploy.Name)
-		err = r.Update(context.TODO(), found)
-		if err != nil {
-			return reconcile.Result{}, err
-		}
-	}
-	return reconcile.Result{}, nil
+	log.Printf("instance spec: %v\n", instance.Spec)
+	return reconcile.Result{RequeueAfter: defaultSyncPeriod}, nil
 }
