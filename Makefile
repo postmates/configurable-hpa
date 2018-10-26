cf1bbf89561bb4d2ad6026047a13182ebcb6c875
Ivan Glushkov <ivan@postmates.com>
2018-10-27 00:54:21 +0400

    - created a project with kubebuilder - copied replica_calculator* from k8s-1.10.8 - copied part of horizontal.go from k8s-1.5.8 - merged, magically fixed to compile - added needed configuration parameters into API - added some documntation - checked RBAC rules

---
+++ Makefile
@@ -0,0 +1,56 @@
+# Image URL to use all building/pushing image targets
+IMG ?= quay.io/postmates/configurable-hpa:000
+
+all: test manager
+
+# Run tests
+test: generate fmt vet manifests
+	go test ./pkg/... ./cmd/... -coverprofile cover.out
+
+# Build manager binary
+manager: generate fmt vet
+	go build -o bin/manager github.com/postmates/configurable-hpa/cmd/manager
+
+# Run against the configured Kubernetes cluster in ~/.kube/config
+run: generate fmt vet
+	go run ./cmd/manager/main.go
+
+# Install CRDs into a cluster
+install: manifests
+	kubectl apply -f config/crds
+
+# Deploy controller in the configured Kubernetes cluster in ~/.kube/config
+deploy: manifests
+	kubectl apply -f config/crds
+	kustomize build config/default | kubectl apply -f -
+
+# Generate manifests e.g. CRD, RBAC etc.
+manifests:
+	go run vendor/sigs.k8s.io/controller-tools/cmd/controller-gen/main.go all
+
+# Run go fmt against code
+fmt:
+	go fmt ./pkg/... ./cmd/...
+
+# Run go vet against code
+vet:
+	go vet ./pkg/... ./cmd/...
+
+# Generate code
+generate:
+	go generate ./pkg/... ./cmd/...
+
+# Build the docker image
+docker-build: test
+	docker build . -t ${IMG}
+
+# Push the docker image
+docker-push:
+	docker push ${IMG}
+
+# Build the docker image for test
+docker-build-test:
+	docker build . --squash -f Dockerfile.test -t quay.io/postmates/configurable-hpa:test-v1
+
+docker-push-test:
+	docker push quay.io/postmates/configurable-hpa:test-v1
---
+++ pkg/apis/autoscalers/v1beta1/chpa_tools.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2018 Ivan Glushkov.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"fmt"
+)
+
+func (chpa CHPA) String() string {
+	ret := fmt.Sprintf("Spec:%v Status:%v", chpa.Spec, chpa.Status)
+	return ret
+}
+
+func (chpa_spec CHPASpec) String() string {
+	minReplicas := "<nil>"
+	if chpa_spec.MinReplicas != nil {
+		minReplicas = fmt.Sprintf("%v", *chpa_spec.MinReplicas)
+	}
+	targetCPUUtilizationPercentage := "<nil>"
+	if chpa_spec.TargetCPUUtilizationPercentage != nil {
+		targetCPUUtilizationPercentage = fmt.Sprintf("%v", *chpa_spec.TargetCPUUtilizationPercentage)
+	}
+	ret := fmt.Sprintf("{Ref:%v/%v DFWS:%v UFWS:%v SULF:%v SULM:%v T:%v MinR:%v MaxR:%v TCPUUP:%v}",
+		chpa_spec.ScaleTargetRef.Kind,
+		chpa_spec.ScaleTargetRef.Name,
+		chpa_spec.DownscaleForbiddenWindowSeconds,
+		chpa_spec.UpscaleForbiddenWindowSeconds,
+		chpa_spec.ScaleUpLimitFactor,
+		chpa_spec.ScaleUpLimitMinimum,
+		chpa_spec.Tolerance,
+		minReplicas,
+		chpa_spec.MaxReplicas,
+		targetCPUUtilizationPercentage)
+	return ret
+}
+
+func (chpa_status CHPAStatus) String() string {
+	curCPU := "<nil>"
+	if chpa_status.CurrentCPUUtilizationPercentage != nil {
+		curCPU = fmt.Sprintf("%v", *chpa_status.CurrentCPUUtilizationPercentage)
+	}
+	lastScaleTime := "<nil>"
+	if chpa_status.LastScaleTime != nil {
+		lastScaleTime = fmt.Sprintf("%v", *chpa_status.LastScaleTime)
+	}
+	ret := fmt.Sprintf("{LST:%v CR:%v DR:%v CCPUUP:%v}", lastScaleTime, chpa_status.CurrentReplicas, chpa_status.DesiredReplicas, curCPU)
+	return ret
+}
